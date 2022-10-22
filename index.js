27fc392130648b67057a1d18809ef011a51e7c66
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-22 12:57:23 +0300

    setTimeout function

---
+++ index.js
@@ -0,0 +1,10 @@
+// Synchronous system example
+console.log(" I");
+console.log(" eat");
+
+setTimeout(() => {
+  console.log(" ice cream");
+}, 1000);
+
+console.log(" with");
+console.log(" a spoon");
