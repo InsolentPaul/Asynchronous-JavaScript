17dc13d83ba6a443f409356322c01e0202e31dd1
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-22 13:08:42 +0300

    Callback

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // Synchronous system example
-console.log(" I");
+/* console.log(" I");
 console.log(" eat");
 
 setTimeout(() => {
   console.log(" ice cream");
-}, 1000);
+}, 1000); // async function
 
 console.log(" with");
-console.log(" a spoon");
+console.log(" a spoon"); */
+
+// Callbacks
+function one(call_two) {
+  call_two();
+  console.log("Step 1 Complete. Step 2 Caliing.");
+}
+function two() {
+  console.log("Step 2 is here");
+}
+
+one(two);
